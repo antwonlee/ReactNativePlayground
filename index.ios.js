0c372a9ce926bd5cb10a245066ccd6884431cde8
Anthony Lee <anthonylee@gatech.edu>
2017-08-30 15:44:01 -0500

    Study what each line is doing

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -17,14 +11,10 @@ export default class ReactNativePlayground extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Thrive Ministry!
+          Thrive Ministry
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          We love Global Women.
         </Text>
       </View>
     );
