2e11d78ddbbb2d046bf2eb3ff566680e98301b1a
Anthony Lee <anthonylee@gatech.edu>
2017-08-30 22:24:50 -0500

    Change to functional

--- src/components/VolunteerList.js
+++ src/components/VolunteerList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const VolunteerList = () => {
-  return (
-    <View>
-      <Text>Volunteer List</Text>
-    </View>
-  );
-};
+class VolunteerList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Volunteer List</Text>
+      </View>
+    );
+  }
+}
 
 export default VolunteerList;
